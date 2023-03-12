6c9d28077498fb8e5dbcdf0971ef8c346f89caac
ali_s <alischeich67@gmail.com>
2023-03-13 00:21:30 +0100

    Movie class extended and dummy data provided

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Genre.java
@@ -37,5 +37,5 @@ public enum Genre {
 
     WAR,
 
-    WESTERN
+    ANIMATION, WESTERN
 }
--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 
@@ -18,6 +19,20 @@ public class Movie {
         this.genres = genres;
     }
 
+    @Override
+    public boolean equals(Object object){
+        if(object == null){
+            return false;
+        }
+        if(object == this){
+            return true;
+        }
+        if(!(object instanceof Movie other)){
+            return false;
+        }
+        return this.title.equals(other.title) && this.description.equals(other.description) && this.genres.equals(other.genres);
+    }
+
     public String getTitle() {
         return title;
     }
@@ -26,9 +41,32 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres() { return genres; }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
+
+        movies.add(new Movie(
+                "The Dawg",
+                "Movie about a cute dawg.",
+                Arrays.asList(Genre.ANIMATION, Genre.FAMILY)
+        ));
+        movies.add(new Movie(
+                "TitansAdventure",
+                "A cute Titan on an adventure.",
+                Arrays.asList(Genre.ANIMATION, Genre.ADVENTURE)
+        ));
+        movies.add(new Movie(
+                "Generic Movie Title",
+                "Generic movie description.",
+                Arrays.asList(Genre.MYSTERY)
+        ));
+        movies.add(new Movie(
+                "The Movie",
+                "The description.",
+                Arrays.asList(Genre.COMEDY)
+        ));
 
         return movies;
     }
